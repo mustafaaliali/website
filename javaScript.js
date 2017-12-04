347e02f05c1e7c89b6cef2b94ca129069d54887a
sami <mustafa-alizada@hotmail.co.uk>
2017-12-04 18:13:39 +0000

    commit 14
    Create a contac us form + the validation using HTML and CSS ans aslo add some image to the website content

--- javaScript.js
+++ javaScript.js
@@ -58,4 +58,6 @@ function carousel() {
     if (slideIndex > x.length) {slideIndex = 1}
     x[slideIndex-1].style.display = "block";
     setTimeout(carousel, 3000); // Change image every 3 seconds
+
 }
+

c247cbf9edc527dc1f3fe639f3d20f0bb5220d51
sami <mustafa-alizada@hotmail.co.uk>
2017-12-13 20:13:12 +0000

    commit 26
    Tested to see all the the layout are responisve plus add more anmation keyfram at the buttom of the page

--- javaScript.js
+++ javaScript.js
@@ -50,7 +50,26 @@ function carousel() {
     setTimeout(carousel, 3000); // Change image every 3 seconds
 }
 function myFunction() {
-    document.getElementById("menu").classList.toggle("show");
+    document.getElementById("my-navbar").classList.toggle("show");
    }
+   
+   // this script opens the dropdown menu 
+   // it also closes the dropdown menu when a link is clicked on or when clicking outside the menu
+   
+   window.onclick = function(event) {
+    if (!event.target.matches('.dropdown-button')) {
+   
+     var dropdowns = document.getElementsByClassName("dropdown-content");
+     var i;
+     for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+       openDropdown.classList.remove('show');
+      }
+     }
+    }
+   }
+
+
    
 

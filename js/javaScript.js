9da2d43f30381670ae9ff8353c65fae4a61c0c9d
sami <mustafa-alizada@hotmail.co.uk>
2017-12-29 23:22:53 +0000

    commit 75
    add it more css and html

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,4 +1,3 @@
-
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -9,68 +8,75 @@ function myFunction() {
 }
 
 var slideIndex = 1;
-    showSlides(slideIndex);
+showSlides(slideIndex);
 
-    function plusSlides(n) {
-      showSlides(slideIndex += n);
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
     }
-
-    function currentSlide(n) {
-      showSlides(slideIndex = n);
+    if (n < 1) {
+        slideIndex = slides.length
     }
-
-    function showSlides(n) {
-      var i;
-      var slides = document.getElementsByClassName("mySlides");
-      var dots = document.getElementsByClassName("dot");
-      if (n > slides.length) {slideIndex = 1}
-      if (n < 1) {slideIndex = slides.length}
-      for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none";
-      }
-      for (i = 0; i < dots.length; i++) {
-          dots[i].className = dots[i].className.replace(" active", "");
-      }
-      slides[slideIndex-1].style.display = "block";
-      dots[slideIndex-1].className += " active";
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
 
-    var slideIndex = 0;
+var slideIndex = 0;
 carousel();
 
 function carousel() {
     var i;
     var x = document.getElementsByClassName("mySlides");
     for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
+        x[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1}
-    x[slideIndex-1].style.display = "block";
+    if (slideIndex > x.length) {
+        slideIndex = 1
+    }
+    x[slideIndex - 1].style.display = "block";
     setTimeout(carousel, 3000); // Change image every 3 seconds
 }
+
 function myFunction() {
     document.getElementById("my-navbar").classList.toggle("show");
-   }
-   
-   // this script opens the dropdown menu 
-   // it also closes the dropdown menu when a link is clicked on or when clicking outside the menu
-   
-   window.onclick = function(event) {
-    if (!event.target.matches('.dropdown-button')) {
-   
-     var dropdowns = document.getElementsByClassName("dropdown-content");
-     var i;
-     for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-       openDropdown.classList.remove('show');
-      }
-     }
-    }
-   }
+}
 
-   var hands = [];
+// this script opens the dropdown menu 
+// it also closes the dropdown menu when a link is clicked on or when clicking outside the menu
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown-button')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+var hands = [];
 hands.push(document.querySelector('#secondhand > *'));
 hands.push(document.querySelector('#minutehand > *'));
 hands.push(document.querySelector('#hourhand > *'));
@@ -79,7 +85,7 @@ var cx = 100;
 var cy = 100;
 
 function shifter(val) {
-  return [val, cx, cy].join(' ');
+    return [val, cx, cy].join(' ');
 }
 
 var date = new Date();
@@ -94,20 +100,13 @@ hands[1].setAttribute('to', shifter(minuteAngle + 360));
 hands[2].setAttribute('from', shifter(hoursAngle));
 hands[2].setAttribute('to', shifter(hoursAngle + 360));
 
-for(var i = 1; i <= 12; i++) {
-  var el = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-  el.setAttribute('x1', '100');
-  el.setAttribute('y1', '30');
-  el.setAttribute('x2', '100');
-  el.setAttribute('y2', '40');
-  el.setAttribute('transform', 'rotate(' + (i*360/12) + ' 100 100)');
-  el.setAttribute('style', 'stroke: #ffffff;');
-  document.querySelector('svg').appendChild(el);
+for (var i = 1; i <= 12; i++) {
+    var el = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+    el.setAttribute('x1', '100');
+    el.setAttribute('y1', '30');
+    el.setAttribute('x2', '100');
+    el.setAttribute('y2', '40');
+    el.setAttribute('transform', 'rotate(' + (i * 360 / 12) + ' 100 100)');
+    el.setAttribute('style', 'stroke: #ffffff;');
+    document.querySelector('svg').appendChild(el);
 }
-   
-   
-   
-
-
-   
-
